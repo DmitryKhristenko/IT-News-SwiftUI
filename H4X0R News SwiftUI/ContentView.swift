21ceb543adc6be1ba86d931c6740cbde5758222e
Dmitry Khristenko <dmitrykhrist@gmail.com>
2022-09-29 22:27:46 +0300

    Initial Commit

---
+++ H4X0R News SwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  H4X0R News SwiftUI
+//
+//  Created by Дмитрий Х on 29.09.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ H4X0R News SwiftUI/H4X0R_News_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0R_News_SwiftUIApp.swift
+//  H4X0R News SwiftUI
+//
+//  Created by Дмитрий Х on 29.09.22.
+//
+
+import SwiftUI
+
+@main
+struct H4X0R_News_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
